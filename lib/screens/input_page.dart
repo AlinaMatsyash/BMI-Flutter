5d524253ddc4293d3e6728435862f38ca729c0a0
alinka <alina.mathyahs@gmail.com>
2020-06-09 17:55:42 +0300

    all

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -8,6 +8,7 @@ import '../constants.dart';
 import 'results_page.dart';
 import '../components/bottom_button.dart';
 import '../components/round_icon_Button.dart';
+import 'package:dmicallculator/calculator_brain.dart';
 
 enum Gender {
   male,
@@ -209,8 +210,17 @@ class _InputPageState extends State<InputPage> {
             ),
             BottomButton(
               onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ResultsPage()));
+                CalculatorBrain calc =
+                    CalculatorBrain(height: height, weight: weight);
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ResultsPage(
+                              bmiResult: calc.calculateBMI(),
+                              resultText: calc.getResult(),
+                              interpretation: calc.getInterpretation(),
+                            )));
               },
               buttonTitle: 'CALCULATE',
             )
